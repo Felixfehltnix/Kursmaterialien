6a401201d10e6d96e9bec71b30132098b5d130db
Felixfehltnix <backer2g@googlemail.com>
2020-09-11 16:23:09 +0200

    Ordnernamen geändert, Fortschritt

---
+++ 14 - JavaScript im Browser (3)/Teil 07 - Auf eine Checkbox zugreifen/js/main.js
@@ -0,0 +1,61 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const addStudentButton = document.getElementById("add-student")
+    const StudentsList = document.getElementById("students-list")
+    const inputElement = document.getElementById("nameInput")
+    const agbInput = document.getElementById("agbInput")
+
+
+    // const liElements = document.getElementsByTagName("li")           // TODO: warum werden die neuen Elemente nicht entfernt?
+    // for (const liElement of liElements) {
+    //     liElement.addEventListener("click", () => {
+    //         liElement.remove()
+    //     })
+    // }
+
+    let counter = 0
+    addStudentButton.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        if (agbInput.checked === false){
+            alert("bitte AGB lesen!")
+            return
+        }
+
+
+        const text = inputElement.value
+
+        if (text === "") {
+            return
+        }
+
+        const strongElement = document.createElement("strong")
+        strongElement.appendChild(document.createTextNode(text))
+
+        const liElement = document.createElement("li")
+        liElement.classList.add("list-group-item")
+        counter++
+        if (counter % 2 === 0) {
+            liElement.style.backgroundColor = "grey"
+        } else {
+            if (counter % 2 === 1){
+            liElement.style.backgroundColor = "blue"
+            }
+        }
+        liElement.appendChild(strongElement)
+
+        StudentsList.appendChild(liElement)
+
+        inputElement.value = ""
+
+        const liElements = document.getElementsByTagName("li")           // TODO: warum werden die alten Elemente "nicht" entfernt?
+        for (const liElement of liElements) {
+            liElement.addEventListener("click", () => {
+                liElement.remove()
+            })
+        }
+    })
+
+
+})
