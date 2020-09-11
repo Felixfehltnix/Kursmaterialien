08ae8e8463eaed86d669a22edde3db0678ab8239
Felixfehltnix <backer2g@googlemail.com>
2020-09-11 13:24:24 +0200

    Ordnernamen, .appendchild benutzt

--- 14 - JavaScript im Browser (3)/Teil 02 -Elemente hinzufügen/js/main.js
+++ 14 - JavaScript im Browser (3)/Teil 02 -Elemente hinzufügen/js/main.js
@@ -1,14 +1,20 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addStudentButton = document.getElementById("add-student")
 
-  addStudentButton.addEventListener("click", () => {
-    const liElement = document.createElement("li")
-    liElement.innerText = "Hallo Welt"
-    liElement.classList.add("list-group-item")
+    const addStudent = document.getElementById("add-student")
 
-    const ulElement = document.querySelector("ul.list-group")
-    ulElement.appendChild(liElement)
-  })
-}) 
+    let studentCounter = 0
+
+    addStudent.addEventListener("click", () => {
+
+        const liElement = document.createElement("li");
+        liElement.innerText = "Schueler " + studentCounter.toString(10)
+        liElement.classList.add("list-group-item")
+        studentCounter++
+
+        const ulElement = document.querySelector("ul.list-group")
+        ulElement.appendChild(liElement)
+
+    })
+})                                  // TODO: warum wurde die letzte klammer nicht als Error angezeigt?
